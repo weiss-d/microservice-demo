1343cf3c3c85cbe727c697ee30105996d11411eb
Simon Weiss <wonder@simonweiss.space>
2020-10-07 13:05:36 +0300

    🚧 Initial project structure.

---
+++ noxfile.py
@@ -0,0 +1,49 @@
+import tempfile
+
+import nox
+
+
+nox.options.sessions = "lint", "tests"
+
+locations = "microservice_demo", "tests", "noxfile.py"
+
+
+def install_with_constraints(session, *args, **kwargs):
+    with tempfile.NamedTemporaryFile() as requirements:
+        session.run(
+            "poetry",
+            "export",
+            "--dev",
+            "--format=requirements.txt",
+            f"--output={requirements.name}",
+            external=True,
+        )
+        session.install(f"--constraint={requirements.name}", *args, **kwargs)
+
+
+@nox.session(python=["3.8"])
+def lint(session):
+    args = session.posargs or locations
+    install_with_constraints(
+        session, "flake8"
+    )
+    session.run("flake8", *args)
+
+
+@nox.session(python="3.8")
+def black(session):
+    args = session.posargs or locations
+    install_with_constraints(session, "black")
+    session.run("black", *args)
+@nox.session(python="3.8")
+
+
+@nox.session(python=["3.8"])
+def tests(session):
+    args = session.posargs
+    session.run("poetry", "install", "--no-dev", external=True)
+    install_with_constraints(
+        session, "pytest"
+    )
+    session.run("pytest", *args)
+
